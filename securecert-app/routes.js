1be433ebb38ce08966219317730a9d78cffb18d4
siddhigangulikar <siddhigangulikar@gmail.com>
2019-05-03 23:06:33 +0530

    First commit

---
+++ securecert-app/routes.js
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: Apache-2.0
+var cors = require('cors');
+var cert = require('./controller.js');
+
+module.exports = function(app){
+
+  app.get('/get_cert/:id', cors(), function(req, res){
+    cert.get_cert(req, res);
+  });
+  app.post('/addNewCertificate', cors(), function(req, res){
+    cert.addNewCertificate(req, res);
+  });
+  app.get('/get_all_cert', cors(), function(req, res){
+    cert.get_all_cert(req, res);
+  });
+  app.post('/transfer_cert/:certificate_id/transferName', cors(), function(req, res){
+    cert.transfer_cert(req, res);
+  });
+  app.get('/get_student/:id', cors(), function(req, res){
+    cert.get_student(req, res);
+  });
+  app.post('/addNewStudent', cors(), function(req, res){
+    cert.addNewStudent(req, res);
+  });
+  app.get('/Login:id1/:id2', cors(), function(req, res){
+    cert.Login(req, res);
+  });
+}
